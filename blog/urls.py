506106340692cf0e067d392af2addfeb00451474
Isaac A <iasberry@ivytech.edu>
2023-10-09 20:44:23 -0400

    Fixed typo

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('pst/<int:pk>/', views.post_dtail, name='post_detail'),
+    path('pst/<int:pk>/', views.post_detail, name='post_detail'),
 ]
