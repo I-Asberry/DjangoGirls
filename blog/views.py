d010a4ba92c594519b08dd51d0c4633922d9421e
Isaac A <iasberry@ivytech.edu>
2023-10-09 21:14:19 -0400

    Forms and stuff

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +10,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if form.is_valid():
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
